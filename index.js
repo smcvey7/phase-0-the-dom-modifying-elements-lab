1d6e3ce921a5c7e5e68d24541b78fada9f33f6e9
Steven McVey <smcvey1210@gmail.com>
2022-08-16 07:54:45 -0400

    commit tree 4ab56933084fd5a518ac36bac55686e69ece3a7f

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.id="victory";
+
+newHeader.textContent="Steven is the champion!";
